c1c44711c625d38f2a7de3b8ccd55bf89fa7b273
EmilioRivera <emilio.rivera@hotmail.com>
2018-01-12 19:57:21 -0500

    Working solution for GV

--- gv.js
+++ gv.js
@@ -4,60 +4,49 @@ let args = process.argv.slice(2);
 let min_heap = require('min-heap')
 let filePath = args[0]
 
-let createWord = (characters) => {
+let decompose_word = (characters) => {
     let m = _.reduce(characters, (res, val, key) => {
         res[val] = (res[val] || (res[val] = 0)) + 1
         return res
     }, {})
     return m
 }
-
-let w = createWord("word")
-console.log(w)
-
 let fs = require('fs'), util = require('util'), stream = require('stream')
-
-let list_of_word_index = 1
 let bib_index = {}
+let list_of_word_index = 1
 let process_dic = (words_path, seeds) => {
     let seeds_decomposition = seeds.map((word,index) => {
         return {
             original: word,
-            decomp: createWord(word),
-            position: index
+            decomp: decompose_word(word),
+            position: index,
+            creation_indexes: []
         }
     })
-    let s = fs.createReadStream("liste_des_mots.txt", { encoding: 'utf8' })
+    let s = fs.createReadStream(words_path, { encoding: 'utf8' })
         .pipe(split())
         .on('data', (line) => {
-            /* 
-            console.log(`[${++list_of_word_index}] - ${line}`)
-            let fisrt_letter = line[0]
-            if (!_.has(bib_index, fisrt_letter)) {
-                bib_index[fisrt_letter] = {
-                    first: list_of_word_index,
-                    last: list_of_word_index,
-                }
-            }
-            bib_index[fisrt_letter].last = list_of_word_index
-
-            ++list_of_word_index */
             // Current line contain0s a word from dictionnary
-            let curr_word_map = createWord(line)
+            let curr_word_map = decompose_word(line)
             for (let i = 0; i < seeds_decomposition.length; i++) {
-                const elem = seeds_decomposition[i]
+                // Since mergeWith mutates the object we need to save it and restore it later
+                const elem = _.cloneDeep(seeds_decomposition[i])
                 let d = _.mergeWith(elem.decomp , curr_word_map, (a,b) => (a || 0) - b)
-                if (_.every(_.values(d), (v) => v > 0)){
-                    // the current word can be constructed from "elem.decomp"
-                    // TODO: Write the index "list_of_word_index" at line i
-                    console.log(`${line} can be constructed from ${elem.original}`)
+                if (_.every(_.values(d), (v) => v >= 0)){
+                    // console.log(`${line} can be constructed from ${elem.original}`)
+                    seeds_decomposition[i].creation_indexes.push(list_of_word_index)
                 }
             }
+            ++list_of_word_index
+            process.stdout.write("Processed " + list_of_word_index + "\r")
         }).on('end', async () => {
+            process.stdout.write("\nDone\n")
             // console.log(bib_index)
             // let words = await parseInput()
             // We have an index containing the first letters
-            console.log('Done')
+            seeds_decomposition.forEach(element => {
+                process.stdout.write(element.creation_indexes.join('_')+"\n")
+            });
         })
 }
 
@@ -76,10 +65,10 @@ let parseInput = () => {
             number_of_seeds++
         })
         .on('end', () => {
-            process_dic("", seeds)
+            process_dic("liste_des_mots.txt", seeds)
         })
-        .on('err', () => {
-            console.log(err)
+        .on('err', (v) => {
+            console.log(err,v)
         })
 }
 
