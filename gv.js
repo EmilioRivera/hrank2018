cb4f71971399f0892eda1aab56c550dc515f7a9e
EmilioRivera <emilio.rivera@hotmail.com>
2018-01-13 11:51:47 -0500

    Performnance enhancement by not using lodash

--- gv.js
+++ gv.js
@@ -1,53 +1,49 @@
-let _ = require('lodash')
-let fs = require('fs'),
-    util = require('util'),
-    stream = require('stream'),
-    split = require('split2')
-const { performance } = require('perf_hooks');
+let fs = require('fs')
 let args = process.argv.slice(2);
+
 let file_path = args[0]
-let begin_time = performance.now()
-let end_time = ""
 let decompose_word = (characters) => {
-    return _.reduce(characters, (res, val, key) => {
+    return characters.split('').reduce((res, val, key) => {
         res[val] = (res[val] || (res[val] = 0)) + 1
         return res
     }, {})
 }
 
-let list_of_word_index = 1
-let process_dic = (words_path, seeds) => {
+let isWordCreatableFrom = (seed_decomposition, actual_word) => {
+    for (const key in actual_word) {
+        if (actual_word.hasOwnProperty(key)) {
+            // Check if seed at least has the value
+            if (!seed_decomposition.hasOwnProperty(key))
+                return false
+            if (seed_decomposition[key] < actual_word[key])
+                return false
+        }
+    }
+    return true
+}
+
+let process_dictionary = (words_path, seeds) => {
+    let answer_array = []
     let seeds_decomposition = seeds.map((word, index) => {
         return {
-            original: word,
             decomposition: decompose_word(word),
             position: index,
-            creation_indexes: []
+            // creation_indexes: []
         }
     })
-    let s = fs.createReadStream(words_path, { encoding: 'utf8' })
-        .pipe(split()).on('data', (line) => {
-            let curr_word_map = decompose_word(line)
-            seeds_decomposition.forEach(element => {
-                const clone = _.cloneDeep(element)
-                let d = _.mergeWith(clone.decomposition, curr_word_map, (a, b) => (a || 0) - b)
-                if (_.every(_.values(d), (v) => v >= 0))
-                    element.creation_indexes.push(list_of_word_index)
-            });
-            ++list_of_word_index
-        }).on('end', () => {
-            end_time = performance.now()
-            process.stdout.write("Took " + (end_time - begin_time) + "milliseconds\n")
-            seeds_decomposition.forEach(seed => { process.stdout.write(seed.creation_indexes.join(' ') + "\n") });
-        })
+    let lines = fs.readFileSync(words_path, { encoding: 'utf8' }).split('\r\n')
+    lines.forEach((word_from_dictionnary, i) => {
+        let word_decomposition = decompose_word(word_from_dictionnary)
+        seeds_decomposition.forEach(element => {
+            if (isWordCreatableFrom(element.decomposition, word_decomposition)) {
+                (answer_array[element.position] || (answer_array[element.position] = [])).push(i + 1)
+                // element.creation_indexes.push(i+1) // Index is zero-based whereas we want 1-based index
+            }
+        });
+    })
+    answer_array.forEach((v) => { process.stdout.write(v.join(' ') + '\n') })
+    // seeds_decomposition.forEach(seed => { process.stdout.write(seed.creation_indexes.join(' ') + "\n") });
 }
 
-let parseInput = () => {
-    let seeds = []
-    fs.createReadStream(file_path, { encoding: 'utf8' }).pipe(split())
-        .on('data', (seed) => {seeds.push(seed)})
-        .on('end', () => { process_dic("liste_des_mots.txt", seeds)})
-        .on('err', (v) => { console.log(err, v) })
-}
-
-parseInput()
+let seeds = fs.readFileSync(file_path, { encoding: 'utf8' }).split('\r\n')
+process_dictionary("liste_des_mots.txt", seeds)
