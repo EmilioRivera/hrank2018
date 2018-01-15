b58da70a51cbc1ea7b70c789e5245ee76b38e03d
EmilioRivera <emilio.rivera@hotmail.com>
2018-01-15 08:54:31 -0500

    All sorts of ways to validate in comments

--- GV_CPP/GV/GV/main.cpp
+++ GV_CPP/GV/GV/main.cpp
@@ -9,9 +9,9 @@
 #include <chrono>
 #include <cstdio>
 #include <algorithm>
-#include <bitset>
+#include <numeric>
 
-#define DEBUG 1
+#define DEBUG 0
 
 using namespace std;
 
@@ -27,34 +27,70 @@ std::wstring s2ws(const std::string& t_str)
 
 class WordDecomposition {
 public:
-	WordDecomposition(wstring chainOfLetters) {
+	WordDecomposition(wstring chainOfLetters, bool track = false) {
 		for (size_t i = 0; i < chainOfLetters.length(); i++) {
-			// Non existant values are ok since they will take the default value of int (0)
-			decomposition[(int)chainOfLetters[i]]++;
+			// Non existant vaues are ok since they will take the default value of int (0)
+			auto c = chainOfLetters[i];
+			//cmap[(int)c] += 1;
+			if (!(decomposition.count(c) > 0))
+				decomposition[c] = 1;
+			else
+				decomposition[c] += 1;
 		}
-		verifyCanCreate(chainOfLetters);
+		//original = chainOfLetters;
+		/*sorted = original;
+		sort(sorted.begin(), sorted.end());*/
+		//verifyCanCreate(chainOfLetters);
 	}
 
 	~WordDecomposition() {
-		decomposition.clear();
+		//decomposition.clear();
 	}
-	inline bool canCreateWord(const WordDecomposition& wordDecomposition) {
-		for (auto const& letter : wordDecomposition.decomposition) {
-			int countDiff = decomposition[letter.first] - letter.second;
-			if (countDiff < 0)
+	inline bool canCreateWord(WordDecomposition* wordDecomposition){
+		/*if (sorted.length() != original.length()) {
+			wcerr << "Mutation in string" << endl;
+		}
+		wstring cpy(sorted);
+		int index = 0;
+		for (auto it = wordDecomposition->sorted.begin(); it < wordDecomposition->sorted.end(); ++it, ++index) {
+			auto found = cpy.find(*it);
+			if (found == string::npos)
+				return false;
+			cpy.erase(cpy.begin()+found);
+		}
+		return true;*/
+		//#pragma omp parallel for reduction(&&:x)
+		//for (int i = 0; i < 256; i++) {
+		//	//x = x && (( this->cmap[i] - wordDecomposition->cmap[i]) >= 0);
+		//	if (cmap[i] < wordDecomposition->cmap[i])
+		//		return false;
+		//	/*if (val < 0)
+		//		return true;*/
+		//}
+		//return true;
+
+		for (auto const& letter : wordDecomposition->decomposition) {
+			if (!(decomposition.count(letter.first) > 0))
+				return false;
+			//int initial = decomposition[letter.first];
+			//int countDiff = initial - letter.second;
+			if (this->decomposition[letter.first] < letter.second)
 				return false;
 		}
-		auto myRep = getWstring(*this);
+		/*auto myRep = getWstring(*this);
 		auto otherRep = getWstring(wordDecomposition);
 		sort(myRep.begin(), myRep.end());
 		sort(otherRep.begin(), otherRep.end());
 		if (myRep.compare(otherRep) != 0) {
 			wcerr << "Wrong decomposition ERROR" << endl;
-		}
+		}*/
 		return true;
 	}
 private:
-	map<const int, int> decomposition;
+	map<const wchar_t, int> decomposition;
+	//int cmap[256] = { 0 };
+	/*wstring original;
+	wstring sorted;
 	static wstring getWstring(const WordDecomposition& wd) {
 		wstring recomposition;
 		for (auto const& letter : wd.decomposition) {
@@ -83,15 +119,15 @@ private:
 		if (sortedInput.compare(sortedDecomposition) != 0) {
 			wcerr << "Wrong decomposition ERROR" << endl;
 		}
-	}
+	}*/
 };
 
-const bool USE_CHECKPOINTS = false;
+const bool USE_CHECKPOINTS = true;
 
-void writeSolution(int seedCounter, std::vector<int> * answer_array);
+void writeSolution(int seedCounter, std::vector<std::vector<int>*>& answer_array);
 void decomposeDictionary(std::vector<WordDecomposition *> &wordsFromDictionary);
 void decomposeInput(char * inputFile, std::vector<WordDecomposition *> &seeds, int &seedCounter);
-void findWordDecompositions(std::vector<WordDecomposition *> &seeds, std::vector<WordDecomposition *> &wordsFromDictionary, std::vector<int> * answer_array);
+void findWordDecompositions(std::vector<WordDecomposition *> &seeds, std::vector<WordDecomposition *> &wordsFromDictionary, std::vector<std::vector<int>*>& answer_array);
 void printMetrics(std::chrono::time_point<std::chrono::steady_clock> &start);
 
 // A big sequential and flat solution.
@@ -107,7 +143,7 @@ int main(int argc, char* argv[]) {
 	auto start = chrono::high_resolution_clock::now();
 
 	// Vector containing data from input
-	char* inputFile = "worst.txt";//argv[1];
+	char* inputFile = argv[1];
 	
 	// Build word decomposition for input file
 	//
@@ -119,11 +155,13 @@ int main(int argc, char* argv[]) {
 	//
 	if (USE_CHECKPOINTS) wcout << "Allocating answer array";
 	const int MAX_INPUT_SIZE = 5000;
-	vector<int>* answer_array;
-	answer_array = new vector<int>[seedCounter];
+	vector<vector<int>*> answer_array;
+	answer_array.reserve(seedCounter);
 	for (int j = 0; j < seedCounter; j++) {
 		// Big enough to have everything lined up
-		answer_array->reserve(75000);
+		vector<int>* a = new vector<int>();
+		a->reserve(75000);
+		answer_array.push_back(a);
 	}
 	if (USE_CHECKPOINTS) wcout << " DONE" << endl;
 
@@ -142,10 +180,11 @@ int main(int argc, char* argv[]) {
 
 	// Free objects
 	//
-	for (size_t i = 0; i < seedCounter; i++) {
-		answer_array[i].clear();
+	for (size_t i = 0; i < answer_array.size(); i++) {
+		answer_array[i]->clear();
+		delete answer_array[i];
 	}
-	delete [] answer_array;
+	//delete [] answer_array;
 	for each (WordDecomposition* d in seeds) {
 		delete d;
 	}
@@ -162,14 +201,16 @@ inline void printMetrics(std::chrono::time_point<std::chrono::steady_clock> &sta
 	if (USE_CHECKPOINTS) wcout << diff.count() << endl;
 }
 
-inline void findWordDecompositions(std::vector<WordDecomposition *> &seeds, std::vector<WordDecomposition *> &wordsFromDictionary, std::vector<int> * answer_array)
+inline void findWordDecompositions(std::vector<WordDecomposition *> &seeds, std::vector<WordDecomposition *> &wordsFromDictionary, std::vector<std::vector<int>*>& answer_array)
 {
 	if (USE_CHECKPOINTS) wcout << "Starting decomposing array" << endl;
 	#pragma omp parallel for
 	for (int seedIndex = 0; seedIndex < seeds.size(); seedIndex++) {
-		for (size_t l = 0; l < wordsFromDictionary.size(); l++) {
-			if (seeds[seedIndex]->canCreateWord(*wordsFromDictionary[l]))
-				answer_array[seedIndex].push_back(l + 1);
+		for (int l = 0; l < wordsFromDictionary.size(); l++) {
+			WordDecomposition* el = wordsFromDictionary[l];
+			if (seeds[seedIndex]->canCreateWord(el)) {
+				answer_array[seedIndex]->push_back(l+1);
+			}
 		}
 		if (USE_CHECKPOINTS) wcout << seedIndex << "\r";
 	}
@@ -189,7 +230,7 @@ inline void decomposeInput(char * inputFile, std::vector<WordDecomposition *> &s
 	if (USE_CHECKPOINTS) wcout << "Starting input decomposition...";
 	wstring line;
 	while (getline(inputReader, line)) {
-		WordDecomposition* d = new WordDecomposition(line);
+		WordDecomposition* d = new WordDecomposition(line, true);
 		seeds.push_back(d);
 		++seedCounter;
 	}
@@ -203,39 +244,59 @@ inline void decomposeDictionary(std::vector<WordDecomposition *> &wordsFromDicti
 	// Reserve a 130,000 line array to contain words
 	wordsFromDictionary.reserve(130000);
 
-	wifstream dictionnaryReader("liste_des_mots.txt", ifstream::in);
+	wifstream dictionnaryReader("C:\\f\\liste_des_mots.txt", ifstream::in);
 	dictionnaryReader.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
 	int lineIndex = 1;
 	wstring dictionaryLine;
 	while (getline(dictionnaryReader, dictionaryLine)) {
 		WordDecomposition* lineDecomposition = new WordDecomposition(dictionaryLine);
 		wordsFromDictionary.push_back(lineDecomposition);
+		lineIndex++;
 	}
 	dictionnaryReader.close();
 	if (USE_CHECKPOINTS) wcout << "DONE" << endl;
 }
 
-inline void writeSolution(int seedCounter, std::vector<int> * answer_array)
+inline void writeSolution(int seedCounter, std::vector<std::vector<int>*>& answer_array)
 {
+	const bool useWof = false;
+	wofstream os;
+	FILE* outFile;
+	if (useWof) {
+		os.open("C:\\f\\out_wofstream.txt", ofstream::out);
+		os.imbue(std::locale(std::locale::empty(), new std::codecvt_utf8<wchar_t>));
+	} else
+		outFile = fopen("C:\\f\\out.txt", "w");
+	FILE* statsFile = fopen("C:\\f\\stats.txt", "w");
+	//wstringstream ss;
 	// Use C stdio for faster write speed
-	FILE* outFile = fopen("out.txt", "w");
+	wcout << "Seed counter"  << seedCounter << endl;
 	if (USE_CHECKPOINTS) wcout << "Starting to write to disk ";
-	for (size_t k = 0; k < seedCounter; k++) {
-		stringstream ss(stringstream::out);
-		for (int i = 0; i < answer_array[k].size(); i++) {
-			int p = answer_array[k][i];
-#if DEBUG
-			wcout << p << " ";
-#endif
-			fprintf(outFile, "%d ", p);
+	for (size_t k = 0; k < answer_array.size(); k++) {
+		auto el = answer_array[k];
+		for (int ansIndex = 0; ansIndex < el->size(); ansIndex++) {
+			int p = (*el)[ansIndex];
+			//wcout << p << " ";
+			if (useWof)
+				os << p << " ";
+			else
+				fprintf(outFile, "%d ", p);
 		}
-#if DEBUG
-		wcout << endl;
-#endif
+		fprintf(statsFile, "%d\n", el->size());
+		//wcout << endl;
 		// Prevent last empty line
-		if (k + 1 < seedCounter)
+		//if (k + 1 < answer_array.size())
+		if (useWof)
+			os << endl;
+		else
 			fprintf(outFile, "\n");
 	}
-	fclose(outFile);
+	if (useWof)
+		os.close();
+	else
+		fclose(outFile);
+	fclose(statsFile);
+	/*wstring con = ss.str();
+	os.write(con.c_str(), con.length());*/
 	if (USE_CHECKPOINTS) wcout << " DONE" << endl;
 }
